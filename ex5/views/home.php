0d0cc13b7e830ba2df1a4d9e8d7c03bc3a9c2548
RalStel <benton.stelle@sbcglobal.net>
2018-03-25 15:00:54 -0400

    Initial Commit
    Initial Commit

---
+++ ex5/views/home.php
@@ -0,0 +1,24 @@
+<?php
+require_once('application/config.php');
+include('elements/header.php');?>
+<div class="container">
+	<div class="page-header">
+    <h1>Hello From the View</h1>
+  </div>
+</div>
+<?php
+//echo $u->getEmail();
+//$password = "CIT31300";
+//
+//$passhash = password_hash($password, PASSWORD_DEFAULT);
+//
+//echo $passhash;
+//
+//if(password_verify($password, $passhash)) {
+//    Echo "It's the same";
+//}
+//else {
+//    echo "Nope, not the same";
+//}
+?>
+<?php include('elements/footer.php');?>
